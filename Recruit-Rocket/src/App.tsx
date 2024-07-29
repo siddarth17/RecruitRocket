15486a01f1af2f7b316d1773a955db358cdaad78
Priyansh Bhatter <pbhatter@usc.edu>
2024-07-29 00:17:33 -0700

    companies

--- Recruit-Rocket/src/App.tsx
+++ Recruit-Rocket/src/App.tsx
@@ -6,7 +6,7 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
-import {Home, ForgotPassword, Login, Register} from "./pages";
+import {Home, ForgotPassword, Login, Register, CompanyList} from "./pages";
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
@@ -17,6 +17,8 @@ import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout" 
 import { resources } from "./config/resources";
+import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
 
 
 function App() {
@@ -57,6 +59,11 @@ function App() {
                     </Authenticated>
                     }>
                       <Route index element={<Home />} />
+                      <Route path="/companies" >
+                        <Route index element={<CompanyList />} />
+                        <Route path="new" element={<Create />} />
+                        <Route path="edit/:id" element={<Edit />} />
+                      </Route>
                   </Route>
                 </Routes>
                 <RefineKbar />
